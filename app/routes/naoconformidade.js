ab60ca22c75afa98a45bb2cd8cb75bf21287967c
Rodolfo Cruz <rodolfocruz.ti@gmail.com>
2016-06-11 21:20:27 -0300

    Adicionando melhorias para utilizar o "verbos" http para definir ações =)

--- app/routes/naoconformidade.js
+++ app/routes/naoconformidade.js
@@ -1,6 +1,6 @@
 
 module.exports = function(app){
-	app.get('/naoconformidade/listar', function(req, res){
+	var listaNaoconformidade = function(req, res){
 
 		//Possivel, pois o express-load carregou esta dependencia no arquivo express.js
 		var connection = app.infra.connectionFactory();
@@ -11,31 +11,25 @@ module.exports = function(app){
 		});
 		
 		connection.end();
-	});	
+	};
+	
+	
+	app.get('/naoconformidade/listar', listaNaoconformidade);	
 	
 	app.get('/naoconformidade/input', function(req, res){
 		res.render('naoconformidade/input.ejs');
 	});
 	
-	app.get('/naoconformidade/salvar', function(req, res){
-		console.log('Esta operação somente é possível via POST');
-		res.render('Erro ao processar operação. Esta operação somente é possível via POST!')
-	});
-	
-	app.post('/naoconformidade/salvar', function(req, res){
+	app.post('/naoconformidade', function(req, res){
 		
-		var naoconformidade = req.body;
-		
-		console.log(naoconformidade);
+		var naoconformidade = req.body;			
 		
 		var connection = app.infra.connectionFactory();
 	 
 		var dao = new app.infra.NaoconformidadeDAO(connection);
 		
 		dao.salvar(naoconformidade, function(err, results){
-			dao.listar(function(err, results){
-				res.render('naoconformidade/listagem.ejs', {lista: results});
-			});			
+			res.redirect('/naoconformidade/listar');		
 		});
 	});
 }
