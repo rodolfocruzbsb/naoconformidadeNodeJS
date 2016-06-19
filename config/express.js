00b3e1c3e2207050d2e06c929200ced2bb469a0d
Rodolfo Cruz <rodolfocruz.ti@gmail.com>
2016-06-19 10:03:21 -0300

    Ajustando para ver o log quando fizer deploy no heroku

--- config/express.js
+++ config/express.js
@@ -34,10 +34,10 @@ module.exports = function(){
 		next();
 	});
 	app.use(function(error, req, res, next){
-		if(process.env.NODE_ENV == 'production'){
-			res.status(500).render('erros/500');
-			return;
-		}
+//		if(process.env.NODE_ENV == 'production'){
+//			res.status(500).render('erros/500');
+//			return;
+//		}
 		
 		next(error);
 	});
