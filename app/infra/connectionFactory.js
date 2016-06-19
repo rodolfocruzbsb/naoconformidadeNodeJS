b572cb484b98aca70ea6d55f643828c91335decd
Rodolfo Cruz <rodolfocruz.ti@gmail.com>
2016-06-19 10:36:21 -0300

    Ajustes para ver melhor o LOG

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -4,13 +4,8 @@ var urlConn = process.env.CLEARDB_DATABASE_URL;
 
 var grupos = urlConn ? urlConn.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/) : "";
 
-console.log("Grupos: "+grupos);
-console.log("CLEARDB_DATABASE_URL: "+process.env.CLEARDB_DATABASE_URL);
-
 var connectMYSQL = function() {
 	if (!process.env.NODE_ENV) {
-
-		console.log("DEV");
 		return mysql.createConnection({
 			host : 'localhost',
 			user : 'root',
@@ -19,9 +14,7 @@ var connectMYSQL = function() {
 		});
 	}
 	if (process.env.NODE_ENV = 'production') {
-		console.log("PRODUCAO");
 		return mysql.createConnection({
-			
 			host : grupos[3],
 			user : grupos[1],
 			password : grupos[2],
@@ -30,8 +23,6 @@ var connectMYSQL = function() {
 	}
 
 	if (process.env.NODE_ENV = 'test') {
-		console.log("TEST");
-
 		return mysql.createConnection({
 			host : 'localhost',
 			user : 'root',
